b76904a6e55260ce402f5598b65ede2e61f252d1
student <haris.phnx@gmail.com>
2015-12-10 15:48:05 +0530

    Day9 and Day10 added

---
+++ Day10/advent_10_2.py
@@ -0,0 +1,15 @@
+string, iterations = "1113222113", 1
+
+while(iterations <= 50):
+    temp, i = "", 0
+    while( i < len(string)):
+        count = 1
+        while( i < len(string)-1 and string[i] == string[i+1]):
+            count += 1
+            i += 1
+        temp += str(count) + str(string[i])
+        i += 1
+
+    string = temp
+    iterations += 1
+print len(string)
---
+++ Day9/advent_9_1.py
@@ -0,0 +1,20 @@
+from itertools import permutations
+
+with open("day_9_input.txt") as f:
+    routes = f.readlines()
+
+route_mat, perms, ans, count = [[0 for i in range(8)] for i in range(8)], [''.join(p) for p in permutations('01234567')], 9999, 0
+
+for i in range(8):
+    for j in range(i+1, 8):
+        route_mat[i][j] = int(routes[count].split(' ')[4])
+        route_mat[j][i] = int(routes[count].split(' ')[4])
+        count += 1
+
+for perm in perms:
+    temp = 0
+    for j in range(len(perm)-1):
+        temp += route_mat[int(perm[j])][int(perm[j+1])]
+    if(temp < ans):
+        ans = temp
+print ans
